0c0b51abb11bb385c0eafaddd0dcf3e5d33acbdd
aztec-dev <aztech.dev1@gmail.com>
2024-06-20 11:29:02 +1000

    defined variables

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ Random password generator
 Python version 3.9.11
 Authored: Azariah Pundari or aztec-dev (https://github.com/aztec-dev).
 """
+
+# Import libraries
+import random
+
+def main():
+    # Define the variables
+    MIN_LENGTH = 8
+    characters = "abcdefghijklmnopqrstuvwxyz"
+    capital_characters = characters.upper()
+    numbers = "0123456789"
+    special_characters = "!@#$%^&*()_+"
+    character_sequence = []
+
+
+
+
+if __name__ == "__main__":
+    main()
