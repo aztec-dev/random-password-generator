17b24fe4ef2746abef001ae1a238dafd328498d7
aztec-dev <aztech.dev1@gmail.com>
2024-06-20 11:38:46 +1000

    wip: working password generator

--- main.py
+++ main.py
@@ -6,15 +6,27 @@ Authored: Azariah Pundari or aztec-dev (https://github.com/aztec-dev).
 
 # Import libraries
 import random
+from string import ascii_letters
+from string import digits
+from string import punctuation 
 
 def main():
     # Define the variables
     MIN_LENGTH = 8
-    characters = "abcdefghijklmnopqrstuvwxyz"
-    capital_characters = characters.upper()
-    numbers = "0123456789"
-    special_characters = "!@#$%^&*()_+"
     character_sequence = []
+
+    # Generate the password
+    password_length = int(input("Enter the length of the password: "))
+    if password_length < MIN_LENGTH:
+        print(f"Password length should be at least {MIN_LENGTH} characters.")
+    else:
+        character_sequence.append(ascii_letters)
+        character_sequence.append(digits)
+        character_sequence.append(punctuation)
+        
+    for i in range(password_length):
+        random_sequence = random.choice(character_sequence)
+        print(random.choice(random_sequence), end="")
 
 
 
